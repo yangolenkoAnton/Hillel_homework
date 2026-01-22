254833262d35a34540330438b3abe27dd124ea6b
yangolenkoAnton <yangolenko1995@gmail.com>
2026-01-22 19:20:56 +0200

    HW 2 task 4 - done

--- 01/HW 2 task 4.py
+++ 01/HW 2 task 4.py
@@ -1,5 +1,5 @@
-a = input("enter the price : ")
-b = input("The percent of discount(%): ")
-c = int(a) * int(b) / 100
-d = int (a) - int(c)
+a = int(input("enter the price : "))
+b = int(input("The percent of discount(%): "))
+c = (a) * (b) / 100
+d = (a) - (c)
 print("your price with discount is: ", int(d))
