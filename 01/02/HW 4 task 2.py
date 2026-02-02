e4e8719b4f28f6dcfefbdcd5065c9c95dc219634
yangolenkoAnton <yangolenko1995@gmail.com>
2026-02-02 12:59:23 +0200

    HW 4 task 2 - done

--- 01/02/HW 4 task 2.py
+++ 01/02/HW 4 task 2.py
@@ -2,12 +2,19 @@
 #[6] => 36
 #[] => 0
 
-first_list = [1, 3, 5]
+first_list = []
 second_list = first_list [0::2]
-print(second_list)
-action_1 = 0
-for x in second_list:
-    action_1 =  action_1 + x
-resolt = action_1 * first_list [-1]
-print(action_1)
-print(resolt)
+a = sum(second_list)
+if first_list == []:
+    print(first_list)
+else:
+ b = int(first_list[-1] * a)
+ print(b)
+# спробував трохи інакше + додав умову для first_list наче запрацювало , пробува перший раз через цикл як на відео ви просили , тяжко не врахував про []
+#  а print то для себе залишав щоб бачити як працює :)
+#action_1 = 0
+#for x in second_list:
+    #action_1 =  action_1 + x
+#resolt = action_1 * first_list [-1]
+#print(action_1)
+#print(resolt)
