11a2f7168912c84e1a5496825661927c29dfc383
yangolenkoAnton <yangolenko1995@gmail.com>
2026-02-02 18:32:55 +0200

    HW 4 task 3 - done

--- 01/02/HW 4 task 3.py
+++ 01/02/HW 4 task 3.py
@@ -18,5 +18,5 @@ import random
 my_list = [random.randint(1,100) for i in range(random.randint(3, 10))]
 print(my_list)
 new_list = []
-new_list=((my_list[0], my_list[2], my_list[-2]))
+new_list= [my_list[0], my_list[2], my_list[-2]]
 print(new_list)
