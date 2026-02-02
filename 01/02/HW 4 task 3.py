9d7bee735f050134f7ac248b2ce4bacef0dd53e8
yangolenkoAnton <yangolenko1995@gmail.com>
2026-02-02 18:30:39 +0200

    HW 4 task 3 - done

--- 01/02/HW 4 task 3.py
+++ 01/02/HW 4 task 3.py
@@ -18,8 +18,5 @@ import random
 my_list = [random.randint(1,100) for i in range(random.randint(3, 10))]
 print(my_list)
 new_list = []
-for num in my_list:
-    new_list=((my_list[0], my_list[2], my_list[-2]))
-    if len(new_list) > 2:
-        break
+new_list=((my_list[0], my_list[2], my_list[-2]))
 print(new_list)
