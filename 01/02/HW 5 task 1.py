0c6832c96be60c6e68dae015443d8c5ab8896364
yangolenkoAnton <yangolenko1995@gmail.com>
2026-02-11 18:23:26 +0200

    HW 5 task 1 - done

---
+++ 01/02/HW 5 task 1.py
@@ -0,0 +1,20 @@
+import string
+import keyword
+
+name = input("enter name: ")
+valid = True
+if name and name[0].isdigit():
+    valid = False
+for item in name:
+    if item.isupper():
+        valid = False
+if " " in name:
+    valid = False
+for item in string.punctuation:
+    if item != "_" and item in name:
+        valid = False
+if all( item == "_" for item in name):
+    valid = False
+if name in keyword.kwlist:
+    valid = False
+print(valid)
