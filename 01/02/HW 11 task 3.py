d12834daec628c40a6dcc5863675ad825eddabf5
yangolenkoAnton <yangolenko1995@gmail.com>
2026-03-02 17:32:22 +0200

    HW 11 task 3 - done

---
+++ 01/02/HW 11 task 3.py
@@ -0,0 +1,9 @@
+def is_even(number):
+    last_digit = str(number)[-1]
+    return last_digit in '02468'
+
+
+assert is_even(2494563894038 ** 2) == True, 'Test1'
+assert is_even(1056897 ** 2) == False, 'Test2'
+assert is_even(24945638940387 ** 3) == False, 'Test3'
+print('OK')
