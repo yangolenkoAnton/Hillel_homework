07f3860776d86ba8ad403da6245ba6fc55976903
yangolenkoAnton <yangolenko1995@gmail.com>
2026-03-02 17:16:33 +0200

    HW 10 task 2 - done

---
+++ 01/02/HW 10 task 2.py
@@ -0,0 +1,17 @@
+def first_word(text):
+    word = ""
+    for char in text:
+        if char.isalpha() or char == "'":
+            word += char
+        elif word:
+            break
+    return word
+
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
